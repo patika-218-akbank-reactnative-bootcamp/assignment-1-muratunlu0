797fe9fb68bf76dd674fd31d880bd92150fe4500
Murat Unlu <muratunlu.official@gmail.com>
2022-08-19 19:05:51 +0300

    added stories

---
+++ odevbirincihafta/src/components/Stories.js
@@ -0,0 +1,55 @@
+import React from "react";
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Entypo from 'react-native-vector-icons/Entypo';
+
+import {
+  ScrollView,
+  View,
+  Text,
+  Pressable,
+  TouchableOpacity,
+  Alert,
+  Image,
+} from "react-native";
+import StoryButton from "./StoryButton";
+import SizedBox from "./SizedBox";
+const Stories = () => {
+  return (
+    <View style={{height: 120, flex: 1, backgroundColor: "black"}}>
+    <ScrollView horizontal={true} 
+    showsHorizontalScrollIndicator={false}
+    showsVerticalScrollIndicator={false}
+    contentContainerStyle={{ flexGrow: 1,
+      alignItems: 'center',
+       justifyContent: 'center',
+        paddingLeft: 20,
+         paddingRight: 20, }}>
+        <StoryButton my={true} usernameColor={"gray"} username={"Your story"} resimKaynagi={"https://pbs.twimg.com/profile_images/1407682940881059844/xy_-4i_0_400x400.jpg"} />
+        <SizedBox />
+        <StoryButton my={false} usernameColor={"white"} username={"esraxln_"} resimKaynagi={"https://img.wattpad.com/04ad7d37f06664571ea6476267ec0fe74e0346da/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f64774f52477064625072726b35673d3d2d38362e313537376134323366373833383934343531323432303631373430342e6a7067?s=fit&w=720&h=720"} />
+        <SizedBox />
+        <StoryButton my={false} usernameColor={"white"} username={"berkfalan"} resimKaynagi={"https://foto.haberler.com/galeri/2017/11/01/instagram-in-siradisi-fenomenleri-710426_4267_70_b.jpg"} />
+        <SizedBox />
+        <StoryButton my={false} usernameColor={"white"} username={"poncikbyn"} resimKaynagi={"https://galeri13.uludagsozluk.com/726/sozluk-yazarlarinin-asik-oldugu-kiz-tipi_1873752.jpg"} />
+        <SizedBox />
+        <StoryButton my={false} usernameColor={"white"} username={"zeynpkar"} resimKaynagi={"https://i.pinimg.com/736x/bf/f1/1d/bff11de83086515bdf280818ec8c3458.jpg"} />
+        <SizedBox />
+        <StoryButton my={false} usernameColor={"white"} username={"canylmzc"} resimKaynagi={"https://cdn1.ntv.com.tr/gorsel/uMpPdC6mtUCHkXgRrBY7jA.jpg?width=1000&mode=crop&scale=both"} />
+        <SizedBox />
+        <StoryButton my={false} usernameColor={"white"} username={"esraxln_"} resimKaynagi={"https://img.wattpad.com/04ad7d37f06664571ea6476267ec0fe74e0346da/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f64774f52477064625072726b35673d3d2d38362e313537376134323366373833383934343531323432303631373430342e6a7067?s=fit&w=720&h=720"} />
+        <SizedBox />
+        <StoryButton my={false} usernameColor={"white"} username={"berkfalan"} resimKaynagi={"https://foto.haberler.com/galeri/2017/11/01/instagram-in-siradisi-fenomenleri-710426_4267_70_b.jpg"} />
+        <SizedBox />
+        <StoryButton my={false} usernameColor={"white"} username={"poncikbyn"} resimKaynagi={"https://galeri13.uludagsozluk.com/726/sozluk-yazarlarinin-asik-oldugu-kiz-tipi_1873752.jpg"} />
+        <SizedBox />
+        <StoryButton my={false} usernameColor={"white"} username={"zeynpkar"} resimKaynagi={"https://i.pinimg.com/736x/bf/f1/1d/bff11de83086515bdf280818ec8c3458.jpg"} />
+        <SizedBox />
+        <StoryButton my={false} usernameColor={"white"} username={"canylmzc"} resimKaynagi={"https://cdn1.ntv.com.tr/gorsel/uMpPdC6mtUCHkXgRrBY7jA.jpg?width=1000&mode=crop&scale=both"} />
+    </ScrollView>
+    </View>
+  );
+};
+
+export default Stories;
---
+++ odevbirincihafta/src/components/StoryButton.js
@@ -0,0 +1,101 @@
+import React from "react";
+import {View,Text, StyleSheet, Dimensions, Image} from "react-native";
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+const screenWidth = Dimensions.get("screen").width;
+const screenHeight = Dimensions.get("screen").height;
+
+const StoryButton = ({resimKaynagi, username, usernameColor, my}) => {
+  return (
+    <View style={{display: "flex", flexDirection: "row", justifyContent: 'center'}}>
+
+    <View style={styles.imageContainer}>
+
+    {my == false ? <View style={styles.circle}>
+    <View style={styles.InCircle}>
+    
+      <Image
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+        source={{
+          uri: resimKaynagi,
+        }}
+      />
+
+    </View>
+    </View>: <View style={styles.InCircle}>
+    
+    <Image
+      style={{
+        width: "100%",
+        height: "100%",
+      }}
+      source={{
+        uri: resimKaynagi,
+      }}
+    />
+
+  </View> }
+    
+    </View>
+<Text
+style={{
+  color: usernameColor, marginTop: 67, fontSize: 13,
+}}>{username}
+</Text>
+
+{my == true ? <View style={styles.plusBck}>
+
+    <AntDesign name="plus" size={16} color="white" />
+</View>: null }
+
+</View>
+  );
+};
+
+const styles = StyleSheet.create({
+  plusBck: {
+    padding: 1.5,
+    justifyContent: 'center',
+    position: "absolute",
+    left: 37,
+    top: 37,
+    width: 23,
+    height: 23,
+    borderRadius: 50,
+    borderWidth: 2,
+    backgroundColor: "#09A1D7",
+    overflow: "hidden",
+  },
+  imageContainer: {
+    position: "absolute",
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    backgroundColor: "rgba(0,0,0,0.3)",
+    overflow: "hidden",
+  },
+  circle: {
+    justifyContent: 'center',
+    width: 60,
+    height: 60,
+    borderRadius: 60 / 2,
+    borderWidth: 2.2,
+    borderColor: '#750e94',
+    overflow: 'hidden',
+  },
+  InCircle: {
+    justifyContent: 'center',
+    width: 56,
+    height: 56,
+    borderRadius: 56 / 2,
+    borderWidth: 2.2,
+    borderColor: 'black',
+    overflow: 'hidden',
+  },
+  
+});
+
+export default StoryButton;
